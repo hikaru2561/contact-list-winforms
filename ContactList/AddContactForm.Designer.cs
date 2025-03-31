26f9cea8b9f733454244998494cd2384f13bd383
hikaru2561 <nguyenkimquang2561@gmail.com>
2025-03-31 23:13:08 +0700

    Sửa đổi AddContactForm

--- ContactList/AddContactForm.Designer.cs
+++ ContactList/AddContactForm.Designer.cs
@@ -35,70 +35,89 @@
             this.btnLuu = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(101, 63);
+            this.label1.Location = new System.Drawing.Point(76, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Họ Tên";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(101, 103);
+            this.label2.Location = new System.Drawing.Point(76, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "SĐT";
             // 
             // txtHoTen
             // 
-            this.txtHoTen.Location = new System.Drawing.Point(176, 63);
+            this.txtHoTen.Location = new System.Drawing.Point(132, 51);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtHoTen.Name = "txtHoTen";
-            this.txtHoTen.Size = new System.Drawing.Size(140, 22);
+            this.txtHoTen.Size = new System.Drawing.Size(106, 20);
             this.txtHoTen.TabIndex = 4;
             // 
             // txtSDT
             // 
-            this.txtSDT.Location = new System.Drawing.Point(176, 103);
+            this.txtSDT.Location = new System.Drawing.Point(132, 84);
+            this.txtSDT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSDT.Name = "txtSDT";
-            this.txtSDT.Size = new System.Drawing.Size(140, 22);
+            this.txtSDT.Size = new System.Drawing.Size(106, 20);
             this.txtSDT.TabIndex = 5;
             // 
             // btnLuu
             // 
-            this.btnLuu.Location = new System.Drawing.Point(176, 179);
+            this.btnLuu.Location = new System.Drawing.Point(132, 145);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLuu.Name = "btnLuu";
-            this.btnLuu.Size = new System.Drawing.Size(98, 41);
+            this.btnLuu.Size = new System.Drawing.Size(74, 33);
             this.btnLuu.TabIndex = 6;
             this.btnLuu.Text = "Lưu";
             this.btnLuu.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(176, 151);
+            this.textBox1.Location = new System.Drawing.Point(132, 123);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.Size = new System.Drawing.Size(106, 20);
             this.textBox1.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(101, 154);
+            this.label3.Location = new System.Drawing.Point(76, 125);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Email";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(74, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Thông tin bổ sung";
+            // 
             // AddContactForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 257);
+            this.ClientSize = new System.Drawing.Size(352, 209);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnLuu);
@@ -106,6 +125,7 @@
             this.Controls.Add(this.txtHoTen);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AddContactForm";
             this.Text = "AddContactForm";
             this.ResumeLayout(false);
@@ -121,5 +141,6 @@
         private System.Windows.Forms.Button btnLuu;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
